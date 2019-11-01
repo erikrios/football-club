aaf1131575a72dd16169bad2745282dd068f6231
erikrios <erikriosetiawan49@gmail.com>
2019-11-01 21:05:00 +0700

    Show Favorite Teams

---
+++ app/src/main/java/com/erikriosetiawan/footballclub/adapter/FavoriteTeamsAdapter.kt
@@ -0,0 +1,74 @@
+package com.erikriosetiawan.footballclub.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.erikriosetiawan.footballclub.R
+import com.erikriosetiawan.footballclub.db.Favorite
+import com.squareup.picasso.Picasso
+import org.jetbrains.anko.*
+
+class FavoriteTeamsAdapter(
+    private val favorite: List<Favorite>,
+    private val listener: (Favorite) -> Unit
+) : RecyclerView.Adapter<FavoriteViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
+        return FavoriteViewHolder(
+            TeamUI().createView(
+                AnkoContext.Companion.create(
+                    parent.context,
+                    parent
+                )
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = favorite.size
+
+    override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+        holder.bindItem(favorite[position], listener)
+    }
+}
+
+class TeamUI : AnkoComponent<ViewGroup> {
+
+    override fun createView(ui: AnkoContext<ViewGroup>): View {
+        return with(ui) {
+            linearLayout {
+                lparams(width = matchParent, height = wrapContent)
+                padding = dip(16)
+                orientation = LinearLayout.HORIZONTAL
+
+                imageView {
+                    id = R.id.team_badge
+                }.lparams {
+                    height = dip(50)
+                    width = dip(50)
+                }
+
+                textView {
+                    id = R.id.team_name
+                    textSize = 16f
+                }.lparams {
+                    margin = dip(15)
+                }
+            }
+        }
+    }
+}
+
+class FavoriteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val teamBadge: ImageView = view.find(R.id.team_badge)
+    private val teamName: TextView = view.find(R.id.team_name)
+
+    fun bindItem(favorite: Favorite, listener: (Favorite) -> Unit) {
+        Picasso.get().load(favorite.teamBadge).fit().into(teamBadge)
+        teamName.text = favorite.teamName
+        itemView.setOnClickListener { listener(favorite) }
+    }
+}
--- app/src/main/java/com/erikriosetiawan/footballclub/fragment/FavoriteTeamsFragment.kt
+++ app/src/main/java/com/erikriosetiawan/footballclub/fragment/FavoriteTeamsFragment.kt
@@ -1,26 +1,94 @@
 package com.erikriosetiawan.footballclub.fragment
 
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.erikriosetiawan.footballclub.R
+import com.erikriosetiawan.footballclub.adapter.FavoriteTeamsAdapter
+import com.erikriosetiawan.footballclub.db.Favorite
+import com.erikriosetiawan.footballclub.db.database
+import com.erikriosetiawan.footballclub.view.TeamDetailActivity
+import org.jetbrains.anko.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.select
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.onRefresh
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 /**
  * A simple [Fragment] subclass.
  */
-class FavoriteTeamsFragment : Fragment() {
+class FavoriteTeamsFragment : Fragment(), AnkoComponent<Context> {
+
+    private var favorites: MutableList<Favorite> = mutableListOf()
+    private lateinit var adapter: FavoriteTeamsAdapter
+    private lateinit var listTeam: RecyclerView
+    private lateinit var swipeRefresh: SwipeRefreshLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_teams, container, false)
+        return createView(AnkoContext.Companion.create(requireContext()))
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        adapter = FavoriteTeamsAdapter(favorites) {
+            context?.startActivity<TeamDetailActivity>("id" to "${it.teamId}")
+        }
+
+        listTeam.adapter = adapter
+        swipeRefresh.onRefresh {
+            showFavorite()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showFavorite()
+    }
+
+    override fun createView(ui: AnkoContext<Context>): View = with(ui) {
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(
+                    R.color.colorAccent,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light
+                )
+
+                listTeam = recyclerView {
+                    lparams(width = matchParent, height = wrapContent)
+                    layoutManager = LinearLayoutManager(ctx)
+                }
+            }
+        }
+    }
+
+    private fun showFavorite() {
+        favorites.clear()
+        context?.database?.use {
+            swipeRefresh.isRefreshing = false
+            val result = select(Favorite.TABLE_FAVORITE)
+            val favorite = result.parseList(classParser<Favorite>())
+            favorites.addAll(favorite)
+            adapter.notifyDataSetChanged()
+        }
+    }
 }
