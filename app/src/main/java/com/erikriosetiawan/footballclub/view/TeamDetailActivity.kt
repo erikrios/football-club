621f09783e194ebf89383000675992ffe9045b55
erikrios <erikriosetiawan49@gmail.com>
2019-11-01 21:32:07 +0700

    Delete Teams from Favorite Database

--- app/src/main/java/com/erikriosetiawan/footballclub/view/TeamDetailActivity.kt
+++ app/src/main/java/com/erikriosetiawan/footballclub/view/TeamDetailActivity.kt
@@ -22,7 +22,10 @@ import com.erikriosetiawan.footballclub.presenter.TeamDetailPresenter
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.delete
 import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
 import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
@@ -109,6 +112,9 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
 
         val intent = intent
         id = intent.getStringExtra("id")
+
+        favoriteState()
+
         val request = ApiRepository()
         val gson = Gson()
         presenter = TeamDetailPresenter(this, request, gson)
@@ -143,6 +149,7 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.detail_menu, menu)
         menuItem = menu
+        setFavorite()
         return true
     }
 
@@ -153,7 +160,11 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
                 true
             }
             R.id.add_to_favorite -> {
-                addToFavorite()
+                if (isFavorite) removeFromFavorite() else addToFavorite()
+
+                isFavorite = !isFavorite
+                setFavorite()
+
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -173,6 +184,35 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
             swipeRefresh.snackbar("Added to favorite").show()
         } catch (e: SQLiteConstraintException) {
             swipeRefresh.snackbar(e.localizedMessage).show()
+        }
+    }
+
+    private fun removeFromFavorite() {
+        try {
+            database.use {
+                delete(Favorite.TABLE_FAVORITE, "(TEAM_ID = {id})", "id" to id)
+            }
+            swipeRefresh.snackbar("Removed to favorite").show()
+        } catch (e: SQLiteConstraintException) {
+            swipeRefresh.snackbar(e.localizedMessage).show()
+        }
+    }
+
+    private fun setFavorite() {
+        if (isFavorite)
+            menuItem?.getItem(0)?.icon =
+                ContextCompat.getDrawable(this, R.drawable.ic_added_to_favorites)
+        else
+            menuItem?.getItem(0)?.icon =
+                ContextCompat.getDrawable(this, R.drawable.ic_add_to_favorites)
+    }
+
+    private fun favoriteState() {
+        database.use {
+            val result = select(Favorite.TABLE_FAVORITE)
+                .whereArgs("(TEAM_ID = {id})", "id" to id)
+            val favorite = result.parseList(classParser<Favorite>())
+            if (!favorite.isEmpty()) isFavorite = true
         }
     }
 }
